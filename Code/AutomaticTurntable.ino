b34c5fe72ccca62f9c50b576a3efd5e0eb9c455f
Patrick <pnelson1@ycp.edu>
2021-05-28 16:31:55 -0400

    added sensor test and updated code for new pinout

--- Code/AutomaticTurntable.ino
+++ Code/AutomaticTurntable.ino
@@ -6,27 +6,45 @@ const int stepsPerRevolution = 2048;
 
 // The vertical stepper motor controls the up and down movements of the tonearm, such as lifting the stylus off of the 
 // record or setting it down
-#define STEPPER_VERTICAL_PIN1 2
-#define STEPPER_VERTICAL_PIN2 3
-#define STEPPER_VERTICAL_PIN3 4
-#define STEPPER_VERTICAL_PIN4 5
+#define STEPPER_VERTICAL_PIN1 0
+#define STEPPER_VERTICAL_PIN2 1
+#define STEPPER_VERTICAL_PIN3 2
+#define STEPPER_VERTICAL_PIN4 3
 Stepper VerticalTonearmMotor = Stepper(stepsPerRevolution, STEPPER_VERTICAL_PIN1, STEPPER_VERTICAL_PIN3, STEPPER_VERTICAL_PIN2, STEPPER_VERTICAL_PIN4);
 
 // The horizontal stepper motor controls the left and right movements of the tonearm, such as positioning it at a
 // horizontal axis so the vertical movement can place the tonearm at the correct location
-#define STEPPER_HORIZONTAL_PIN1 10
-#define STEPPER_HORIZONTAL_PIN2 11
-#define STEPPER_HORIZONTAL_PIN3 12
-#define STEPPER_HORIZONTAL_PIN4 13
+#define STEPPER_HORIZONTAL_PIN1 4
+#define STEPPER_HORIZONTAL_PIN2 5
+#define STEPPER_HORIZONTAL_PIN3 6
+#define STEPPER_HORIZONTAL_PIN4 7
 Stepper HorizontalTonearmMotor = Stepper(stepsPerRevolution, STEPPER_HORIZONTAL_PIN1, STEPPER_HORIZONTAL_PIN3, STEPPER_HORIZONTAL_PIN2, STEPPER_HORIZONTAL_PIN4);
 
-// Left/right buttons for clockwise/counterclockwise test movements. These exist for troubleshooting/testing purposes only.
-#define LEFT_BUTTON 6
-#define RIGHT_BUTTON 7
+// Buttons that the user can press to execute certain movements
+#define HOME_BUTTON 8
+#define PLAY_BUTTON 10
+#define PAUSE_BUTTON 12
 
-// These LEDs light up when moving one direction or another. They exist for troubleshooting/testing purposes only.
-#define MOV_LED_L 8
-#define MOV_LED_R 9
+// Indicator lights so we can tell what movement the turntable is currently executing
+#define HOME_STATUS_LED 9
+#define PLAY_STATUS_LED 11
+#define PAUSE_STATUS_LED 13
+
+// Positioning sensors for the vertical tonearm movement. The lower limit switch designates "home" for the tonearm's vertical position
+// The upper sensor is the "pause" position, and will allow the tonearm to go higher to engage with the gearing that moves horizontally
+#define VERTICAL_UPPER_SENSOR A0
+#define VERTICAL_LOWER_LIMIT A1
+
+// Positioning sensors for the horizontal tonearm movement. Each one represents a different point that the horizontal tonearm could be in,
+// and will allow the various funcrtions to move it in the correct direction depending on where it currently is.
+#define HORIZONTAL_HOME_SENSOR A2 // Tells the tonearm where "home" is horizontally
+#define HORIZONTAL_PLAY_SENSOR A3 // Tells the tonearm where to drop the stylus down on the record
+#define HORIZONTAL_PICKUP_SENSOR A4 // Tells the tonearm where the end of the record is
+
+// This tells us whether the user flipped the 3-way switch to "automatic" or "manual"
+// Automatic will automatically home the turntable at the end of the record, while manual
+// will not. Even with manual selected, the "home," "pause" and "play" buttons will still work
+#define AUTO_OR_MANUAL A5
 
 // This is an internal step count for the motors, so we know how many steps it has taken
 int32_t verticalStepCount = 0;
@@ -49,56 +67,58 @@ void setup() {
   pinMode(STEPPER_HORIZONTAL_PIN4, OUTPUT);
   HorizontalTonearmMotor.setSpeed(movementRPM);
   
-  pinMode(LEFT_BUTTON, INPUT);
-  pinMode(RIGHT_BUTTON, INPUT);
+  pinMode(HOME_BUTTON, INPUT);
+  pinMode(PLAY_BUTTON, INPUT);
+  pinMode(PAUSE_BUTTON, INPUT);
 
-  pinMode(MOV_LED_L, OUTPUT);
-  pinMode(MOV_LED_R, OUTPUT);
+  pinMode(HOME_STATUS_LED, OUTPUT);
+  pinMode(PLAY_STATUS_LED, OUTPUT);
+  pinMode(PAUSE_STATUS_LED, OUTPUT);
 
   Serial.begin(SERIAL_SPEED);
 
-  homeBothAxes();
+  bool verticalUpperIsPausedOrEngaged = digitalRead(VERTICAL_UPPER_SENSOR);
+
+  // If the tonearm is currently lifted up to a "paused" or "moving" state, then we want to home it
+  if(verticalUpperIsPausedOrEngaged) {
+    homeBothAxes();
+  }
+
+  // Otherwise, we only want to home the vertical axis, because the stylus may already be on a record
+  else {
+    homeVerticalAxis();
+  }
 }
 
 void loop() {
-  bool leftButtonStatus = digitalRead(LEFT_BUTTON);
-  bool rightButtonStatus = digitalRead(RIGHT_BUTTON);
+
+  // IR SENSOR TEST
+  bool sensorStatus = digitalRead(VERTICAL_UPPER_SENSOR);
 
   // If the left button is pressed, move clockwise and light the left LED
-  if(leftButtonStatus && !rightButtonStatus) {
-    digitalWrite(MOV_LED_L, HIGH);
-    digitalWrite(MOV_LED_R, LOW);
-
+  if(sensorStatus) {
     VerticalTonearmMotor.step(1);
     verticalStepCount++;
 
     HorizontalTonearmMotor.step(1);
     horizontalStepCount++;
   }
-
-  // If the right button is pressed, move counterclockwise and light the right LED
-  else if (!leftButtonStatus && rightButtonStatus) {
-    digitalWrite(MOV_LED_L, LOW);
-    digitalWrite(MOV_LED_R, HIGH);
-
-    VerticalTonearmMotor.step(-1);
-    verticalStepCount--;
-
-    HorizontalTonearmMotor.step(-1);
-    horizontalStepCount--;
-  }
-  
-  // No movement, so shut the LEDs off
-  else {
-    digitalWrite(MOV_LED_L, LOW);
-    digitalWrite(MOV_LED_R, LOW);
-  }
 }
 
-// This handles homing the tonearm, which must be done every time the application is started
-void homeBothAxes() {
+// This is only used when first powering on to verify the tonearm is not being moved
+// If it is found that the pause sensor is not tripped, then it will know that the tonearm
+// was previously lifted, and should be homed instead
+void homeVerticalAxis() {
+    // TODO: Implement
 
-  // Now that both axes have been homed, the step count can be reset to 0 for each axis
+    // Now that the vertical axis has been homed, the step count can be reset to 0
+    verticalStepCount = 0;
+}
+
+void homeBothAxes() {
+  //TODO: Implement
+
+  // Now that the axes have been homed, their step counts can be reset to 0
   verticalStepCount = 0;
   horizontalStepCount = 0;
 }
