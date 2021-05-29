4766b62f46baa1cb4437072fc8962d31915b22a1
Patrick <pnelson1@ycp.edu>
2021-05-29 13:05:54 -0400

    removed unnecessary operations, updated comment grammar

--- Code/AutomaticTurntable.ino
+++ Code/AutomaticTurntable.ino
@@ -4,7 +4,7 @@
 #define STEPS_PER_REVOLUTION 2048
 
 // The vertical stepper motor controls the up and down movements of the tonearm, such as lifting the stylus off of the 
-// record or setting it down
+// record or setting it down.
 #define STEPPER_VERTICAL_PIN1 0
 #define STEPPER_VERTICAL_PIN2 1
 #define STEPPER_VERTICAL_PIN3 2
@@ -12,56 +12,56 @@
 Stepper VerticalTonearmMotor = Stepper(STEPS_PER_REVOLUTION, STEPPER_VERTICAL_PIN1, STEPPER_VERTICAL_PIN3, STEPPER_VERTICAL_PIN2, STEPPER_VERTICAL_PIN4);
 
 // The horizontal stepper motor controls the left and right movements of the tonearm, such as positioning it at a
-// horizontal axis so the vertical movement can place the tonearm at the correct location
+// horizontal axis so the vertical movement can place the tonearm at the correct location.
 #define STEPPER_HORIZONTAL_PIN1 4
 #define STEPPER_HORIZONTAL_PIN2 5
 #define STEPPER_HORIZONTAL_PIN3 6
 #define STEPPER_HORIZONTAL_PIN4 7
 Stepper HorizontalTonearmMotor = Stepper(STEPS_PER_REVOLUTION, STEPPER_HORIZONTAL_PIN1, STEPPER_HORIZONTAL_PIN3, STEPPER_HORIZONTAL_PIN2, STEPPER_HORIZONTAL_PIN4);
 
-// Buttons that the user can press to execute certain movements
+// Buttons that the user can press to execute certain movements.
 #define HOME_BUTTON 8
 #define PLAY_BUTTON 10
 #define PAUSE_BUTTON 12
 
-// Indicator lights so we can tell what the turntable is currently doing
+// Indicator lights so we can tell what the turntable is currently doing.
 #define MOVEMENT_STATUS_LED 11
 #define PAUSE_STATUS_LED 13
 
-// Positioning sensors for the vertical tonearm movement. The lower limit switch designates "home" for the tonearm's vertical position
-// The upper sensor is the "pause" position, and will allow the tonearm to go higher to engage with the gearing that moves horizontally
+// Positioning sensors for the vertical tonearm movement. The lower limit switch designates "home" for the tonearm's vertical position.
+// The upper sensor is the "pause" position, and will allow the tonearm to go higher to engage with the gearing that moves horizontally.
 #define VERTICAL_PICKUP_LIMIT A0
 #define VERTICAL_HOME_LIMIT A1
 
 // Positioning sensors for the horizontal tonearm movement. Each one represents a different point that the horizontal tonearm could be in,
 // and will allow the various funcrtions to move it in the correct direction depending on where it currently is.
-#define HORIZONTAL_HOME_SENSOR A2 // Tells the tonearm where "home" is horizontally
-#define HORIZONTAL_PLAY_SENSOR A3 // Tells the tonearm where to drop the stylus down on the record
-#define HORIZONTAL_PICKUP_SENSOR A4 // Tells the tonearm where the end of the record is
+#define HORIZONTAL_HOME_SENSOR A2 // Tells the tonearm where "home" is horizontally.
+#define HORIZONTAL_PLAY_SENSOR A3 // Tells the tonearm where to drop the stylus down on the record.
+#define HORIZONTAL_PICKUP_SENSOR A4 // Tells the tonearm where the end of the record is.
 
 // This is used to engage the horizontal gears for movement. This is needed so that the gears aren't engaged
 // when a record is playing or any other times, otherwise the record would not be able to move the tonearm
 // very well...
 #define HORIZONTAL_GEARING_SOLENOID 9
 
-// This tells us whether the user flipped the 3-way switch to "automatic" or "manual"
+// This tells us whether the user flipped the 3-way switch to "automatic" or "manual."
 // Automatic will automatically home the turntable at the end of the record, while manual
-// will not. Even with manual selected, the "home," "pause" and "play" buttons will still work
+// will not. Even with manual selected, the "home," "pause" and "play" buttons will still work.
 #define AUTO_OR_MANUAL_SWITCH A5 // Auto = high; manual = low
 
 // The motors used in this project are 28BYJ-48 stepper motors, which I've found to cap at 11 RPM 
-// before becoming too unreliable. 8 or 9 I've found to be a good balance for speed and reliability at 5v DC
+// before becoming too unreliable. 8 or 9 I've found to be a good balance for speed and reliability at 5v DC.
 #define MOVEMENT_RPM 8
 
 // These are timeouts used for error checking, so the hardware doesn't damage itself.
 // Essentially, if the steps exceed this number and the motor has not yet reached its
-// destination, an error has occurred
+// destination, an error has occurred.
 #define PLAY_TIMEOUT_STEPS 50
-#define HOME_TIMEOUT_STEPS 50 // TODO: Determine values for these fields
+#define HOME_TIMEOUT_STEPS 50 // TODO: Determine values for these fields.
 #define PAUSE_TIMEOUT_STEPS 500
 
 // Step counts used for error checking. We will have an idea of how many steps a movement should take,
-// so here we are keeping track of those so we know it doesn't exceed the limits defined above
+// so here we are keeping track of those so we know it doesn't exceed the limits defined above.
 int verticalStepCount = 0;
 int horizontalStepCount = 0;
 
@@ -87,19 +87,19 @@ void setup() {
 
   pinMode(HORIZONTAL_GEARING_SOLENOID, OUTPUT);
 
-  // If the turntable is turned on to "automatic," then home the whole tonearm
+  // If the turntable is turned on to "automatic," then home the whole tonearm.
   if(digitalRead(AUTO_OR_MANUAL_SWITCH)) {
     homeTonearm();
   }
 
-  // Otherwise, we only want to home the vertical axis if it isn't already homed, which will drop the tonearm in its current location
-  else if(!digitalRead(VERTICAL_HOME_LIMIT)) {
+  // Otherwise, we only want to home the vertical axis, which will drop the tonearm in its current location.
+  else {
     homeVerticalAxis();
   }
 }
 
 // This sits and waits for any of the command buttons to be pressed.
-// As soon as a button is pressed, the corresponding command routine is executed
+// As soon as a button is pressed, the corresponding command routine is executed.
 void loop() {
   if(digitalRead(PLAY_BUTTON)) {
     playRoutine();
@@ -115,20 +115,20 @@ void loop() {
 }
 
 // This is only used when first powering on in manual mode to verify that the tonearm
-// drops down in place, if it was previously shut off between the sensor and home switch
-// The home status LED will light for the duration of this command
+// drops down in place, if it was previously shut off between the sensor and home switch.
+// The home status LED will light for the duration of this command.
 void homeVerticalAxis() {
 
   // We always want to make sure the vertical step count starts at 0 so there aren't leftover steps
-  // from a previous function and the turntable is set to an error state early
+  // from a previous function and the turntable is set to an error state early.
   verticalStepCount = 0;
 
-  // Move the motor down until it bumps into the limit, or timeout is reached
+  // Move the motor down until it bumps into the limit, or timeout is reached.
   while(!digitalRead(VERTICAL_HOME_LIMIT))
   {
     VerticalTonearmMotor.step(-1);
 
-    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable
+    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable.
     if(verticalStepCount++ >= PAUSE_TIMEOUT_STEPS || digitalRead(VERTICAL_PICKUP_LIMIT)) {
       setErrorState(ErrorCode::VerticalHomeError);
     }
@@ -142,8 +142,8 @@ void homeTonearm() {
   verticalStepCount = 0;
   horizontalStepCount = 0;
 
-  // TODO: Implement
-  
+  // TODO: Implement.
+
   digitalWrite(MOVEMENT_STATUS_LED, LOW);
 }
 
@@ -151,11 +151,11 @@ void pauseAndWaitUntilUnpaused() {
   digitalWrite(PAUSE_STATUS_LED, HIGH);
   verticalStepCount = 0;
 
-  // Move the motor up until the pick-up limit is hit
+  // Move the motor up until the pick-up limit is hit.
   while(!digitalRead(VERTICAL_PICKUP_LIMIT)) {
     VerticalTonearmMotor.step(1);
     
-    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable
+    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable.
     if(verticalStepCount++ >= PAUSE_TIMEOUT_STEPS || digitalRead(VERTICAL_HOME_LIMIT)) {
       setErrorState(ErrorCode::VerticalPickupError);
     }
@@ -164,16 +164,16 @@ void pauseAndWaitUntilUnpaused() {
   verticalStepCount = 0;
   releaseCurrentFromBothMotors();
 
-  // Wait for the user to unpause
+  // Wait for the user to unpause.
   while(!digitalRead(PAUSE_BUTTON)) {
     delay(1);
   }
 
-  // Move the motor down until the vertical home limit is hit
+  // Move the motor down until the vertical home limit is hit.
   while(!digitalRead(VERTICAL_HOME_LIMIT)) {
     VerticalTonearmMotor.step(-1);
     
-    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable
+    // If the limit switch isn't hit in the expected time, or the wrong limit switch is hit, error the turntable.
     if(verticalStepCount++ >= PAUSE_TIMEOUT_STEPS || digitalRead(VERTICAL_PICKUP_LIMIT)) {
       setErrorState(ErrorCode::VerticalHomeError);
     }
@@ -188,12 +188,12 @@ void playRoutine() {
   verticalStepCount = 0;
   horizontalStepCount = 0;
 
-  // TODO: Implement
+  // TODO: Implement.
 
   digitalWrite(MOVEMENT_STATUS_LED, LOW);
 }
 
-// This is used to release current from the motors so they aren't drawing power when not in use
+// This is used to release current from the motors so they aren't drawing power when not in use.
 void releaseCurrentFromBothMotors() {
   digitalWrite(STEPPER_VERTICAL_PIN1, LOW);
   digitalWrite(STEPPER_VERTICAL_PIN2, LOW);
@@ -211,33 +211,33 @@ void releaseCurrentFromBothMotors() {
 // The user will have to restart the turntable if this occurs.
 void setErrorState(ErrorCode errorCode) {
 
-  // Turn off all motors and LEDs
+  // Turn off all motors and LEDs.
   releaseCurrentFromBothMotors();
   digitalWrite(PAUSE_STATUS_LED, LOW);
   digitalWrite(MOVEMENT_STATUS_LED, LOW);
 
   switch(errorCode) {
 
-    // Slow-blink pause LED
+    // Slow-blink pause LED.
     case ErrorCode::VerticalHomeError:
       blinkLed(PAUSE_STATUS_LED, 1000);
 
-    // Fast-blink pause LED
+    // Fast-blink pause LED.
     case ErrorCode::VerticalPickupError:
       blinkLed(PAUSE_STATUS_LED, 150);
 
-    // Slow-blink movement LED
+    // Slow-blink movement LED.
     case ErrorCode::HorizontalHomeError:
       blinkLed(MOVEMENT_STATUS_LED, 1000);
 
-    // Fast-blink movement LED
+    // Fast-blink movement LED.
     case ErrorCode::PlayError:
       blinkLed(MOVEMENT_STATUS_LED, 150);
   }
 }
 
 // Blinks a specific LED indefinitely.
-// For use only with error codes
+// For use only with error codes.
 void blinkLed(int led, int interval) {
   while(true) {
     digitalWrite(led, LOW);
