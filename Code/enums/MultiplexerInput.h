a5311e4eb4dad2a783581756d7e790c8f417d146
Patrick <pnelson1@ycp.edu>
2021-11-27 19:56:47 -0500

    changed pins on mux

--- Code/enums/MultiplexerInput.h
+++ Code/enums/MultiplexerInput.h
@@ -4,52 +4,70 @@
 // One of the motors that adjusts the position of the tonearm.
 enum MultiplexerInput : uint8_t {
 
+    // Positioning sensor for the vertical tonearm movement. This is the lower sensor, indicating the "play"/"home" vertical position of the tonearm.
+    // S1
+    VerticalLowerLimit = 0,
+    
+    // Positioning sensor for the vertical tonearm movement. This is the upper sensor, indicating the "pause" vertical position of the tonearm.
+    // S2
+    VerticalUpperLimit = 1,
+    
     // The "play"/"home" button, which will move the tonearm (horizontally) to the play sensor if the play sensor is HIGH, or 
-    // move the tonearm (horizontally) to the home sensor if the play sensor is LOW.
-    PlayHomeButton = 0,
+    // move the tonearm (horizontally) to the home sensor if the play sensor is LOW. 
+    // S3
+    PlayHomeButton = 2,
 
     // The "pause" button, which will either lift the tonearm up (if it is currently down), or set the tonearm down (if it is currently up).
-    PauseButton = 1,
-
-    // Positioning sensor for the vertical tonearm movement. This is the upper sensor, indicating the "pause" vertical position of the tonearm.
-    VerticalUpperLimit = 2,
-
-    // Positioning sensor for the vertical tonearm movement. This is the lower sensor, indicating the "play"/"home" vertical position of the tonearm.
-    VerticalLowerLimit = 3,
-
-    // The "home" position optical sensor, which indicates what is considered "home" for the tonearm.
-    HorizontalHomeOpticalSensor = 4,
+    // S4
+    PauseButton = 3,
 
     // The 7" "play" position optical sensor, which indicates where the tonearm would drop to begin playing a 7" record.
-    HorizontalPlay7InchOpticalSensor = 5,
+    // S5
+    HorizontalPlay7InchOpticalSensor = 4,
 
     // The 10" "play" position optical sensor, which indicates where the tonearm would drop to begin playing a 10" record.
-    HorizontalPlay10InchOpticalSensor = 6,
+    // S6
+    HorizontalPlay10InchOpticalSensor = 5,
 
     // The 12" "play" position optical sensor, which indicates where the tonearm would drop to begin playing a 12" record.
-    HorizontalPlay12InchOpticalSensor = 7,
+    // S7
+    HorizontalPlay12InchOpticalSensor = 6,
+
+    // The "home" position optical sensor, which indicates what is considered "home" for the tonearm.
+    // S8
+    HorizontalHomeOpticalSensor = 7,
 
     // This, used together with RecordSizeSelector2, tells the software which record size it should be looking for.
     // If this is HIGH while RecordSizeSelector2 is LOW, the software will use the 7" optical sensor.
     // If both this and RecordSizeSelector2 are LOW, the software will use the 10" optical sensor.
+    // S9
     RecordSizeSelector1 = 8,
 
     // This, used together with RecordSizeSelector1, tells the software which record size it should be looking for.
     // If this is HIGH while RecordSizeSelector1 is LOW, the software will use the 12" optical sensor.
     // If both this and RecordSizeSelector2 are LOW, the software will use the 10" optical sensor.
+    // S10
     RecordSizeSelector2 = 9,
 
     // The "pickup" position optical sensor, which indicates the location the tonearm must be at in order for the homing routine to be executed.
     // Ideally, this would be at the inner deadwax of the record.
+    // S11
     HorizontalPickupOpticalSensor = 10,
 
     // This is the switch used to determine whether the user turned on the turntable in automatic or manual mode.
     // Automatic: The tonearm will return to "home" at the end of a record.
     // Manual: The tonearm will not return to "home" at the end of a record. All buttons still have their same functionality in "automatic" mode.
+    // S12
     AutoManualSwitch = 11,
 
     // This is an optical sensor used to detect what speed the turntable is currently spinning at.
-    TurntableSpeedSensor = 12
+    // S13
+    TurntableSpeedSensor = 12,
+
+    // This is a command pin that can only be set high by the automatic changer. It is only intended to be used when the turntable is in error,
+    // otherwise its status is meaningless.
+    // S14
+    ClearErrorAndHome = 13
 };
 
 #endif
