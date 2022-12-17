cf34254aa58d6775d9af71fc6fe04553ba31a33b
Patrick <pnelson1@ycp.edu>
2022-12-17 11:54:57 -0500

    Finish PCB 0.5 - still need to do BOM

--- Code/enums/ArduinoPin.h
+++ Code/enums/ArduinoPin.h
@@ -12,8 +12,8 @@ enum ArduinoPin : uint8_t {
     // when uploading to the Arduino while it is connected to the rest of the turntable controller board.
     ReservedSerial1 = 1,
 
-    // This is the multiplexer pin that we read values from.
-    MuxOutput = 2,
+    // This is the pin used to select which motor we are moving, using the demultiplexers.
+    MotorAxisSelector = 2,
 
     // This motor pin (4) channels into a 2-channel demultiplexer, so that either the vertical or horizontal motor receives
     // the pulses. Only one of these motors can ever be moving at once.
@@ -31,49 +31,50 @@ enum ArduinoPin : uint8_t {
     // the pulses. Only one of these motors can ever be moving at once.
     StepperPin1 = 6,
 
-    // This is the pin used to select which motor we are moving, using the demultiplexers.
-    MotorAxisSelector = 7,
+    // One (of three) multiplexer selector pins. This is used (with the other pins) to select which device we want
+    // to read a value from.
+    MuxSelectorC = 7,
 
-    // Setting this HIGH will cause the the horizontal motor clutch to either engage or disengage. This is used with HorizontalClutchMotorDir1
-    // to drive a DC motor through an H-bridge.
-    HorizontalClutchMotorDir2 = 8,
+    // One (of three) multiplexer selector pins. This is used (with the other pins) to select which device we want
+    // to read a value from.
+    MuxSelectorB = 8,
+
+    // One (of three) multiplexer selector pins. This is used (with the other pins) to select which device we want
+    // to read a value from.
+    MuxSelectorA = 9,
+
+    // This is the multiplexer pin that we read values from.
+    MuxOutput = 10,
 
     // Setting this HIGH will cause the the horizontal motor clutch to either engage or disengage. This is used with HorizontalClutchMotorDir2
     // to drive a DC motor through an H-bridge.
-    HorizontalClutchMotorDir1 = 9,
+    HorizontalClutchMotorDir1 = 11,
 
-    // One (of four) multiplexer selector pins. This is used (with the other pins) to select which device we want
-    // to read a value from.
-    MuxSelectorA = 10,
+    // Setting this HIGH will cause the the horizontal motor clutch to either engage or disengage. This is used with HorizontalClutchMotorDir1
+    // to drive a DC motor through an H-bridge.
+    HorizontalClutchMotorDir2 = 12,
 
-    // One (of four) multiplexer selector pins. This is used (with the other pins) to select which device we want
-    // to read a value from.
-    MuxSelectorB = 11,
+    // Pin to enable or disable the turntable motor, meaning that once the turntable crosses the home sensor, and this pin is high, it is free to move.
+    TurntableMotorEnable = 13,
 
-    // One (of four) multiplexer selector pins. This is used (with the other pins) to select which device we want
-    // to read a value from.
-    MuxSelectorC = 12,
+    UNUSED_ARDUINO_A0 = A0,
 
-    UNUSED_ARDUINO_13 = 13,
+    UNUSED_ARDUINO_A1 = A1,
 
     // The "home" or "play" position optical sensor, depending on which direction the tonearm is moving. If the tonearm is moving
     // counter-clockwise, this sensor acts as the "home" sensor, whereas if it is moving clockwise, it acts as the play sensor.
-    HorizontalHomeOrPlayOpticalSensor = A0,
+    HorizontalHomeOrPlayOpticalSensor = A2,
 
     // This is used, in combination with an interrupt, to calculate what speed the turntable is currently spinning at.
-    SpeedSensor = A1,
+    SpeedSensor = A3,
 
     // Sensor A of the pickup optical sensor, which, when tripped so many times within a certain interval (also depending on B), will 
     // trigger the homing routine.
-    PickupEncoderA = A2,
+    PickupEncoderA = A4,
 
     // Sensor B of the pickup optical sensor, which, when tripped so many times within a certain interval (also depending on B), will 
     // trigger the homing routine.
-    PickupEncoderB = A3,
-
-    UNUSED_ARDUINO_A4 = A4,
-
-    UNUSED_ARDUINO_A5 = A5,
+    PickupEncoderB = A5,
 
     // Indicator light so we can tell what the turntable is currently doing. This pin will be set HIGH if the turntable
     // is currently executing the "home" or "play" routine.
--- Code/enums/MultiplexerInput.h
+++ Code/enums/MultiplexerInput.h
@@ -4,27 +4,28 @@
 // One of the motors that adjusts the position of the tonearm.
 enum MultiplexerInput : uint8_t {
 
-    // Positioning sensor for the vertical tonearm movement. This is the lower sensor, indicating the "play"/"home" vertical position of the tonearm.
-    VerticalLowerLimit = 0,
-
-    // The "pause" button, which will either lift the tonearm up (if it is currently down), or set the tonearm down (if it is currently up).
-    PauseButton = 1,
+    UNUSED_MUX_0 = 0,
     
     // The "play"/"home" button, which will move the tonearm (horizontally) to the play sensor if the play sensor is HIGH, or 
     // move the tonearm (horizontally) to the home sensor if the play sensor is LOW. 
-    PlayHomeButton = 2,
+    PlayHomeButton = 1,
+
+    // The "pause" button, which will either lift the tonearm up (if it is currently down), or set the tonearm down (if it is currently up).
+    PauseButton = 2,
+    
+    UNUSED_MUX_3 = 3,
     
     // Positioning sensor for the vertical tonearm movement. This is the upper sensor, indicating the "pause" vertical position of the tonearm.
-    VerticalUpperLimit = 3,
+    VerticalUpperLimit = 4,
 
-    UNUSED_MUX_4 = 4,
+    // This tells us whether the turntable power has been set, or if it's currently turned off.
+    TurntablePowerSet = 5,
+
+    // Positioning sensor for the vertical tonearm movement. This is the lower sensor, indicating the "play"/"home" vertical position of the tonearm.
+    VerticalLowerLimit = 6,
 
     // This pin decides whether we are in automatic or manual mode. To know what each value does, see AutoManualSwitchPosition.h.
-    AutoManualSwitch = 5,
-
-    UNUSED_MUX_6 = 6,
-    
-    UNUSED_MUX_7 = 7
+    AutoManualSwitch = 7
 };
 
 #endif
